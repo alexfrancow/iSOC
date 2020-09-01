af9cb62128bcd4bcdae5c6fd0339e0514fd7effd
Alejandro Franco Vázquez <alejandrofrancovazquez@gmail.com>
2020-09-01 22:55:01 +0200

    Update port_to_mongo.py

--- resources/zabbix/externalscripts/port_to_mongo.py
+++ resources/zabbix/externalscripts/port_to_mongo.py
@@ -1,21 +1,25 @@
 '''
         This script is executed on Zabbix Action, when the discovery found a new port,
-	it send the host ip and the port and update the collection on mongodb.
+        it send the host ip and the port and update the collection on mongodb.
                 @alexfrancow
 
-	https://stackoverflow.com/questions/33189258/append-item-to-mongodb-document-array-in-pymongo-without-re-insertion
-	https://docs.mongodb.com/manual/reference/operator/update/pull/
+        https://stackoverflow.com/questions/33189258/append-item-to-mongodb-document-array-in-pymongo-without-re-insertion
+        https://docs.mongodb.com/manual/reference/operator/update/pull/
 '''
 
 from pymongo import MongoClient
+import requests
 import sys
 import argparse
 
-def add_ports(coll, ip, port):
-	coll.update({'ip': ip}, {'$push': {'ports': port}})
+def add_ports(coll, ip, port, port_w_protocol):
+        coll.update({'ip': ip}, {'$push': {'ports': port}})
+        coll.update({'ip': ip}, {'$set': {'ports_w_protocol.'+port: port_w_protocol}})
 
-def delete_ports(coll, ip, port):
-	coll.update({'ip': ip}, {'$pull': {'ports': port}})
+def delete_ports(coll, ip, port, port_w_protocol):
+        coll.update({'ip': ip}, {'$pull': {'ports': port}})
+        coll.update({'ip': ip}, {'$unset': {'ports_w_protocol.'+port: port_w_protocol}})
+
 
 
 parser = argparse.ArgumentParser(description='')
@@ -26,21 +30,36 @@ args = parser.parse_args()
 
 
 if args.ip and args.portAdd or args.portDelete:
-	client = MongoClient("192.168.1.129",
-		username="alexfrancow",
-		password="abc123",
-		maxPoolSize=50)
-	db = client.iSOC
-	collection = db['assets']
-	myquery = { "ip": args.ip }
-	mydoc = collection.find(myquery)
+        client = MongoClient("192.168.1.129",
+                username="alexfrancow",
+                password="abc123",
+                maxPoolSize=50)
+        db = client.iSOC
+        collection = db['assets']
+        myquery = { "ip": args.ip }
+        mydoc = collection.find(myquery)
 
-	if args.portAdd:
-		portAdd = args.portAdd.split(" ")[3]
-		for x in mydoc:
-			add_ports(collection, x['ip'], portAdd)
+        if args.portAdd:
+                # Zabbix problem example: 'Status of port 80 tcp http'
+                portAdd = args.portAdd.split(" ")[3]
+                portProtocol = args.portAdd.split(" ")[5]
+                for x in mydoc:
+                        add_ports(collection, x['ip'], portAdd, portProtocol)
 
-	else:
-		portDelete = args.portDelete.split(" ")[3]
-		for x in mydoc:
-			delete_ports(collection, x['ip'], portDelete)
+                # Get web info
+                if portProtocol == "http":
+                        req = requests.get("http://"+x['ip']+":"+portAdd, verify=False)
+                        collection.update({'ip': x['ip']}, {'$set': {'web_headers': req.headers}})
+                        content = str(req.text.encode('utf-8'))
+                        collection.update({'ip': x['ip']}, {'$set': {'web_payload': content}})
+
+        else:
+                portDelete = args.portDelete.split(" ")[3]
+                portProtocol = args.portDelete.split(" ")[5]
+                for x in mydoc:
+                        delete_ports(collection, x['ip'], portDelete, portProtocol)
+
+                # Get web info
+                if portProtocol == "http":
+                        collection.update({'ip': x['ip']}, {'$set': {'web_headers': "Null"}})
+                        collection.update({'ip': x['ip']}, {'$set': {'web_payload': "Null"}})
